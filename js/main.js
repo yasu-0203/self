5c05a115a9ee8c7b345008b15accde9e8a644c15
yasu-0203 <hori.yasuhiro.0203@gmail.com>
2021-09-13 21:24:29 +0900

    ドロップダウンメニューの追加（仮）

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ $(function(){
   $('button').click(function() {
     alert('うんこ');
   })
+
+  $('.dropdwn li').hover(function(){
+    $("ul:not(:animated)", this).slideDown();
+}, function(){
+    $("ul.dropdwn_menu",this).slideUp();
+});
 });
